a13be1dd10902070042ba2600b15d9f37c3f6116
Felix Angelov <felangelov@gmail.com>
2022-03-31 17:05:43 -0500

    tweak

--- examples/flutter_counter/lib/main_e2e.dart
+++ examples/flutter_counter/lib/main_e2e.dart
@@ -1,31 +1,5 @@
 import 'package:flutter/widgets.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 import 'app.dart';
 
 void main() => runApp(const CounterApp());
-
-/// In memory hydrated storage impl.
-class InMemoryHydratedStorage extends Storage {
-  final _storage = <String, dynamic>{};
-
-  @override
-  Future<void> clear() async {
-    _storage.clear();
-  }
-
-  @override
-  Future<void> delete(String key) async {
-    _storage.remove(key);
-  }
-
-  @override
-  dynamic read(String key) {
-    return _storage[key];
-  }
-
-  @override
-  Future<void> write(String key, dynamic value) async {
-    _storage[key] = value;
-  }
-}
